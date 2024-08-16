6cfe64fdf48a44f33fdff475469a70423860fa9a
ploosh-alt <95927318+ploosh-alt@users.noreply.github.com>
2024-08-15 23:37:14 -0400

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define UPX_VERSION_HEX      0x040300
-#define UPX_VERSION_STRING   "4.3.0"
-#define UPX_VERSION_STRING4  "skid"
+#define UPX_VERSION_STRING   "meow"
+#define UPX_VERSION_STRING4  "meow"
 #define UPX_VERSION_DATE     "Jun 19th 2024"
 #define UPX_VERSION_DATE_ISO "2024-06-19"
 #define UPX_VERSION_YEAR     "2024"
